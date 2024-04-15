73cb03e33145f1bfb4282e518278674bce9c8d15
Hasireddy <malikireddy.uma@gmail.com>
2024-04-15 14:14:12 +0200

    register form

---
+++ signupForm/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+# from . import forms
+from .forms import RegisterForm
+from .models import Register
+
+# Create your views here.
+
+def register(request):
+    form = RegisterForm()
+    return render(
+        request,
+        "signupForm/register.html",
+        {
+            "form":form
+        })
